624203b8b04a854a0e44672553afc0e99ef278de
Yijiaod <126523311+Yijiaod@users.noreply.github.com>
2023-02-27 17:12:38 -0500

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,12 +1,12 @@
 # LoRA train script by @Akegarasu
 
 # Train data path | 设置训练用模型、图片
-pretrained_model="./sd-models/model.ckpt" # base model path | 底模路径
+pretrained_model="./sd-models/model.safetensors" # base model path | 底模路径
 train_data_dir="./train/aki" # train dataset path | 训练数据集路径
 
 # Train related params | 训练相关参数
-resolution="512,512" # image resolution w,h. 图片分辨率，宽,高。支持非正方形，但必须是 64 倍数。
-batch_size=1 # batch size
+resolution="512,640" # image resolution w,h. 图片分辨率，宽,高。支持非正方形，但必须是 64 倍数。
+batch_size=4 # batch size
 max_train_epoches=10 # max train epoches | 最大训练 epoch
 save_every_n_epochs=2 # save every n epochs | 每 N 个 epoch 保存一次
 network_dim=32 # network dim | 常用 4~128，不是越大越好
@@ -59,7 +59,7 @@ accelerate launch --num_cpu_threads_per_process=8 "./sd-scripts/train_network.py
   --enable_bucket \
   --pretrained_model_name_or_path=$pretrained_model \
   --train_data_dir=$train_data_dir \
-  --output_dir="./output" \
+  --output_dir="/content/drive/MyDrive/Lora/output/" \
   --logging_dir="./logs" \
   --resolution=$resolution \
   --network_module=networks.lora \
